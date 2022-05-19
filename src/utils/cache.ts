e012abfc06dd413253d24970220279dcaaf8957d
dajiangjunok <779503577@qq.com>
2022-05-20 01:34:29 +0800

    feat(add cache class): add cache class

---
+++ src/utils/cache.ts
@@ -0,0 +1,23 @@
+class LocalCache {
+  setCache(key: string, value: any): void {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  getCache(key: string) {
+    const value = window.localStorage.getItem(key)
+
+    if (value) {
+      return JSON.parse(value)
+    }
+  }
+
+  deleteCache(key: string): void {
+    window.localStorage.removeItem(key)
+  }
+
+  clearCache(): void {
+    window.localStorage.clear()
+  }
+}
+
+export default new LocalCache()
